6b8181de430b32cb4fe08101dd650f75da8f0dca
LODS <lodsofcc@gmail.com>
2022-08-31 18:36:13 -0300

    feat: test new logic to create class

--- class-oop.lua
+++ class-oop.lua
@@ -1,49 +1,70 @@
-function Class(tbl)
-    setmetatable(tbl, {
-        __call = function(cls, ...)
-            local self = {}
+-- function Class(tbl)
+--     setmetatable(tbl, {
+--         __call = function(cls, ...)
+--             local self = {}
             
-            setmetatable(self, {
-                __index = cls
-            })
+--             setmetatable(self, {
+--                 __index = cls
+--             })
             
-            self:constructor(...)
+--             self:constructor(...)
 
-            return self
-        end
+--             return self
+--         end
+--     })
+--     return tbl
+-- end
+
+-- function ClassExtends(target, base)
+--     local ClassExtended = {}
+
+--     ClassExtended = base
+--     target.super = base
+
+--     local staticMetatable = setmetatable({__index = base}, base)
+--     setmetatable(ClassExtended, staticMetatable)
+--     local baseMetatable = getmetatable(base)
+    
+--     if (baseMetatable) then
+--         if (type(baseMetatable.__index) == 'function') then
+--             staticMetatable.__index = baseMetatable.__index
+--         end
+--         if (type(baseMetatable.__newindex) == 'function') then
+--             staticMetatable.__newindex = baseMetatable.__newindex
+--         end
+--     end
+
+--     setmetatable(ClassExtended, base)
+--     if (type(base.__index) == 'function') then
+--         ClassExtended.__index = base.__index
+--     end
+--     if (type(base.__newindex) == 'function') then
+--         ClassExtended.__newindex = base.__newindex
+--     end
+--     if (type(base.__tostring) == 'function') then
+--         ClassExtended.__tostring = base.__tostring
+--     end
+
+--     return Class(ClassExtended)
+-- end
+
+local Class = {}
+
+function Class:new()
+    local instance = {}
+    __call = function(cls, ...)
+        setmetatable(instance, {
+            __index = cls
+        })
+
+        Class:constructor(...)
+    end
+    setmetatable(instance, {
+        __index = Class
     })
-    return tbl
+    return instance
 end
 
-function ClassExtends(target, base)
-    local ClassExtended = {}
-
-    ClassExtended = base
-    target.super = base
-
-    local staticMetatable = setmetatable({__index = base}, base)
-    setmetatable(ClassExtended, staticMetatable)
-    local baseMetatable = getmetatable(base)
-    
-    if (baseMetatable) then
-        if (type(baseMetatable.__index) == 'function') then
-            staticMetatable.__index = baseMetatable.__index
-        end
-        if (type(baseMetatable.__newindex) == 'function') then
-            staticMetatable.__newindex = baseMetatable.__newindex
-        end
-    end
-
-    setmetatable(ClassExtended, base)
-    if (type(base.__index) == 'function') then
-        ClassExtended.__index = base.__index
-    end
-    if (type(base.__newindex) == 'function') then
-        ClassExtended.__newindex = base.__newindex
-    end
-    if (type(base.__tostring) == 'function') then
-        ClassExtended.__tostring = base.__tostring
-    end
-
-    return Class(ClassExtended)
+function Class:constructor()
+    iprint(self)
 end
